8add5a60e9e300f1e45229bfc1d2336817fa4123
Anomig <goyvaerts.naomi@hotmail.com>
2025-02-17 19:29:20 +0100

    Components: eerste poging tot basis component in App.js

--- App.js
+++ App.js
@@ -1,16 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, TextInput, View, Image } from 'react-native';
+import { StyleSheet, Text, TextInput, View, Image, ScrollView } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Welcome!</Text>
-      <TextInput  placeholder="Enter your name" />
-      <Image source={require('./assets/favicon.png')} />
-      <StatusBar style="auto" />
-    </View>
+    <ScrollView style={styles.container1}>
+      <View style={styles.container}>
+        <Image source={require('./assets/favicon.png')} />
+        <Text style={styles.artist}>artist</Text>
+        <Text style={styles.title}>title</Text>
+        <StatusBar style="auto" />
+      </View>
+    </ScrollView>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
@@ -18,5 +21,15 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  container1: {
+    backgroundColor: '#fff',
+    paddingTop: 50,
+  },
+  artist: {
+    fontSize: 30,
+  },
+  title: {
+    fontSize: 20,
   },
 });
