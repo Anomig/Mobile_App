17c0703898973e617c360586a661ce19f0d97242
Anomig <goyvaerts.naomi@hotmail.com>
2025-06-12 16:46:43 +0200

    adding a navigation with tabs

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
 
 import HomeScreen from "./screens/HomeScreen.js";
 import ProductOverview from "./screens/ProductOverview.js";
@@ -9,17 +11,26 @@ import BlogScreen from "./screens/BlogScreen.js";
 import BlogDetailScreen from './screens/BlogDetailScreen';
 
 const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+function MainTabs() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Products" component={ProductOverview} />
+      <Tab.Screen name="Blog" component={BlogScreen} />
+    </Tab.Navigator>
+  );
+}
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Products" component={ProductOverview} />
-        <Stack.Screen name="Product" component={ProductDetail} />
-        <Stack.Screen name="Blog" component={BlogScreen} />
-        <Stack.Screen name="BlogDetail" component={BlogDetailScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+      <NavigationContainer>
+        <Stack.Navigator screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="MainTabs" component={MainTabs} />
+          <Stack.Screen name="Product" component={ProductDetail} />
+          <Stack.Screen name="BlogDetail" component={BlogDetailScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
   );
 }
